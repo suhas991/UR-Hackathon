315f8693136933ba5e57d6aa413a0da7d1d66fe3
SUHAS N H <suhas4497@gmail.com>
2022-11-06 08:59:43 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CardWrapper from "./components/CardWrapper";
 import MyNavbar from "./components/MyNavbar";
 import "./index.css";
 import BasicSlider from "./Slider.js";
+import FooterSocial from "./components/Footer";
 export default function App() {
   return (
     <div>
@@ -12,12 +13,16 @@ export default function App() {
         <MyNavbar />
       </div>
       <BasicSlider />
+      {/* <div className="seprator">
+        <h2>PRODUCTS</h2>
+      </div> */}
       <div className="main-cover">
         <Navbar1 />
         <div className="grid-wrap">
           <CardWrapper />
         </div>
       </div>
+      <FooterSocial />
     </div>
   );
 }
