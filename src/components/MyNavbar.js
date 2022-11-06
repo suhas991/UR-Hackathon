d6fc3692ba0506841c7bf4b94026e98484a384a9
SUHAS N H <suhas4497@gmail.com>
2022-11-06 04:45:31 +0000

    Update 5 files

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,47 +1,47 @@
-import { Button, Group, Text, Alert, Modal, Image} from "@mantine/core";
-import {useState } from "react";
+import { Button, Group, Alert, Modal } from "@mantine/core";
+import { useState } from "react";
 import { useVarsContext } from "../contexts/VarsContext";
 
 export default function MyNavbar() {
-    const [walAddress, setWalAddress] = useState("");
-    const [opened, setOpened] = useState(false);
-    const [btnVisibility, setBtnVisibility] = useState(false);
-    const [errMsg, setErrMsg] = useState("")
-    const {provider} = useVarsContext()
+  const [walAddress, setWalAddress] = useState("");
+  const [opened, setOpened] = useState(false);
+  const [btnVisibility, setBtnVisibility] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const { provider } = useVarsContext();
 
-    const conWal = async () =>{
-      try{
-      setWalAddress(await provider.send("eth_requestAccounts", []))
-      setBtnVisibility(true)
+  const conWal = async () => {
+    try {
+      setWalAddress(await provider.send("eth_requestAccounts", []));
+      setBtnVisibility(true);
+    } catch (err) {
+      if (err.code) {
+        setOpened(true);
+        setErrMsg(err.message);
       }
-      catch(err){
-        if(err.code){
-          setOpened(true)
-          setErrMsg(err.message)
-        }
-      } 
     }
+  };
   return (
     <>
-    <Group position="apart" m="16px">
-      <Group>
-        Logo
-      </Group>
+      <Group position="apart" m="16px">
+        <Group>Logo</Group>
 
         <Group>
-            {walAddress}
-            <Button color="orange" disabled={btnVisibility} onClick={conWal}>Connect wallet</Button>
+          {walAddress}
+          <Button color="orange" disabled={btnVisibility} onClick={conWal}>
+            Connect wallet
+          </Button>
         </Group>
+      </Group>
 
-    </Group> 
-
-    <Modal opened={opened} onClose={() => setOpened(false)} title="Metamask - RPC error">
-        
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Metamask - RPC error"
+      >
         <Alert title="Please check your Metamask wallet" color="red">
-        {errMsg}
+          {errMsg}
         </Alert>
-
-    </Modal>
+      </Modal>
     </>
-  )
+  );
 }
